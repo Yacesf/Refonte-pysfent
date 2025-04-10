81e7951bfcca9c95d2c37485225a79e31ad8c3df
Yace Sf <yacine.sf@outlook.fr>
2025-04-10 15:24:14 +0200

    first commit et commencement du header

---
+++ src/components/header.jsx
@@ -0,0 +1,54 @@
+import { useScroll, useMotionValueEvent, motion } from "framer-motion";
+import { useState } from "react";
+
+function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const { scrollY } = useScroll();
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    setIsScrolled(latest > 10);
+  });
+
+  const linkNav = [
+    { name: "Accueil", href: "#home" },
+    { name: "À Propos", href: "#about" },
+    { name: "Projets", href: "#project" },
+    { name: "Contact", href: "#contact" },
+  ];
+
+  return (
+    <header className="px-[3%] fixed top-0 left-0 w-full">
+      <nav className="relative z-50">
+        <motion.ul
+          className={`rounded-full flex items-center justify-around mt-[20px] py-3 px-4 transition-all duration-300 ${
+            isScrolled
+              ? "bg-white/40 backdrop-blur-sm shadow-sm"
+              : "bg-transparent"
+          }`}
+        >
+          {linkNav.map((link, index) => (
+            <li className="relative inline-block" key={index}>
+              <a
+                href={link.href}
+                onClick={() => setActiveIndex(index)}
+                className="relative p-2 text-sm font-semibold text-gray-800"
+              >
+                {activeIndex === index && (
+                  <motion.div
+                    layoutId="pill"
+                    className="absolute inset-0 bg-white rounded-full z-[-1]"
+                    transition={{ type: "spring", stiffness: 500, damping: 30 }}
+                  />
+                )}
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </motion.ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
